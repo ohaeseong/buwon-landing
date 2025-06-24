45f72c38e38dd65750e6aaab9cd1463a2a6f8d08
ohaeseong <74897099+ohaeseong@users.noreply.github.com>
2025-06-25 02:49:35 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000,
-    open: true
-  }
-})
+    host: true,
+    allowedHosts: ["buwon.synology.me"],
+    port: 3001,
+    open: true,
+  },
+});
